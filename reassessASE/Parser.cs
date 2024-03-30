6b53bff15472ea0c809410bfd0a6c38132959909
Luke <l.stanley9244@student.leedsbeckett.ac.uk>
2024-03-30 20:53:23 +0000

    added dictionary to store variable names and values

--- reassessASE/Parser.cs
+++ reassessASE/Parser.cs
@@ -35,5 +35,21 @@ namespace reassessASE
             "star"
         };
 
+        //Dictionary to store variable names and values
+        private Dictionary<string, int> variables = new Dictionary<string, int>();
+
+        // A stack to keep track of nested if statements.
+        private Stack<bool> ifStack = new Stack<bool>();
+
+        //A stack to keep track of the start line numbers of the nested while loops.
+        //private Stack<int> whileStack = new Stack<int>();
+
+        //Dictinary to store loop conditions with their line numbers
+        private Dictionary<int, string> loopConditions = new Dictionary<int, string>();
+
+        //Dictionary to map method names to their definitions
+        private Dictionary<string, MethodDefinition> methodDefinitions = new Dictionary<string, MethodDefinition>();
+
+
     }
 }
