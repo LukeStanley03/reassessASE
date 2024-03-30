4205e03e0940ca2450f558f1682f0bb26d758224
Luke <l.stanley9244@student.leedsbeckett.ac.uk>
2024-03-30 20:47:08 +0000

    added a collection set of valid commands

--- reassessASE/Parser.cs
+++ reassessASE/Parser.cs
@@ -9,5 +9,31 @@ namespace reassessASE
     public class Parser
     {
         private Canvas myCanvas;
+
+        //collection set of valid commands 
+        private HashSet<string> validCommands = new HashSet<string>
+        {
+            "moveto",
+            "drawto",
+            "circle",
+            "triangle",
+            "rectangle",
+            "colour",
+            "fill",
+            "clear",
+            "reset",
+            "square",
+            "run",
+            "red",
+            "green",
+            "blue",
+            "black",
+            "pattern",
+            "complex",
+            "shapes",
+            "randomlines",
+            "star"
+        };
+
     }
 }
