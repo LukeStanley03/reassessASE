7e0a63991c96c69daa83c369d40f6e3241f2c17f
Luke <l.stanley9244@student.leedsbeckett.ac.uk>
2024-03-30 19:51:46 +0000

    added canvas class

---
+++ reassessASE/Canvas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reassessASE
+{
+    public interface ICanvas
+    {
+
+    }
+
+    public class Canvas: ICanvas
+    {
+
+    }
+}
