75436e353c92e1f98d80bc1e6aa6348c2cca8870
Luke <l.stanley9244@student.leedsbeckett.ac.uk>
2024-03-31 15:57:02 +0100

    added the drawto command execute

--- reassessASE/DrawToCommand.cs
+++ reassessASE/DrawToCommand.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace reassessASE
 {
-    internal class DrawToCommand
+    public class DrawToCommand : ICanvasCommand
     {
+        private int x, y;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public DrawToCommand(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        /// <summary>
+        /// Draw to position x,y
+        /// </summary>
+        public void Execute(Canvas canvas)
+        {
+            canvas.DrawTo(x, y);
+            canvas.updateCursor(); // Update cursor position after drawing
+        }
     }
 }
