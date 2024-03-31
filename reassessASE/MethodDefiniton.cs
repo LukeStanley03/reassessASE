92b405b73c7297ac925e98c41e2389638933396e
Luke <l.stanley9244@student.leedsbeckett.ac.uk>
2024-03-31 16:01:09 +0100

    added method definiton class which contains the parameter list and method body

---
+++ reassessASE/MethodDefiniton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reassessASE
+{
+    /// <summary>
+    /// contains the parameter list and method body
+    /// </summary>
+    public class MethodDefinition
+    {
+        public string Name { get; }
+        public List<string> Parameters { get; }
+        public List<string> Body { get; }
+
+        public MethodDefinition(string name, List<string> parameters, List<string> body)
+        {
+            Name = name;
+            Parameters = parameters ?? new List<string>();
+            Body = body;
+        }
+    }
+}
