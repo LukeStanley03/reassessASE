90beaf147e8749b446f61cdc03ec382904dcbdcb
Luke <l.stanley9244@student.leedsbeckett.ac.uk>
2024-03-31 16:15:23 +0100

    added rectangle command and executes

--- reassessASE/RectangleCommand.cs
+++ reassessASE/RectangleCommand.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace reassessASE
 {
-    internal class RectangleCommand
+    public class RectangleCommand : ICanvasCommand
     {
+        private int width;
+        private int height;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public RectangleCommand(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        /// <summary>
+        /// Draw a rectangle with width and height
+        /// </summary>
+        /// <param name="canvas"></param>
+        public void Execute(Canvas canvas)
+        {
+            canvas.Rectangle(width, height);
+        }
     }
 }
