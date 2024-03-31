eb46fe343e3a54b17d451d48b8f4f549aa49e3d7
Luke <l.stanley9244@student.leedsbeckett.ac.uk>
2024-03-31 15:54:00 +0100

    added the draw circle command and the execute function

--- reassessASE/CircleCommand.cs
+++ reassessASE/CircleCommand.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace reassessASE
 {
-    internal class CircleCommand
+    public class CircleCommand : ICanvasCommand
     {
+        private int radius;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="radius"></param>
+        public CircleCommand(int radius)
+        {
+            this.radius = radius;
+        }
+
+        /// <summary>
+        /// Draw the circle with radius 
+        /// </summary>
+        /// <param name="canvas"></param>
+        public void Execute(Canvas canvas)
+        {
+            canvas.Circle(radius);
+        }
     }
 }
