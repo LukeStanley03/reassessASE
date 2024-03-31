cd01b391dc49d7fb51a52183612adee999cce5ce
Luke <l.stanley9244@student.leedsbeckett.ac.uk>
2024-03-31 12:33:04 +0100

    added the instance data for the pens x and y positions and the graphics context

--- reassessASE/Canvas.cs
+++ reassessASE/Canvas.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace reassessASE
 {
@@ -11,10 +13,19 @@ namespace reassessASE
         void Circle(int radius);
     }
 
-    public class Canvas: ICanvas
+    public class Canvas : ICanvas
     {
         //Standard size of canvas
         const int XSIZE = 640;
         const int YSIZE = 480;
+
+
+        //instance data for pen and x, y positions and graphics context
+        //graphics context is the drawing area to draw on
+        Form CallingForm;
+        Graphics g, cursorG;
+        int XCanvasSize, YCanvasSize;
+        Pen pen;
+        public int xPos, yPos; // pen position when drawing
     }
 }
