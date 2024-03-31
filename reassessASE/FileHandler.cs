2c76e8901d54f877fb8793e9eb4e35c5c719ae64
Luke <l.stanley9244@student.leedsbeckett.ac.uk>
2024-03-31 13:04:11 +0100

    added a writetofile which will write to the file

--- reassessASE/FileHandler.cs
+++ reassessASE/FileHandler.cs
@@ -36,5 +36,28 @@ namespace reassessASE
                 throw new GPLexception("ERROR: Cannot read the file. " + ex.Message);
             }
         }
+
+        /// <summary>
+        /// Writing the contents to the file
+        /// </summary>
+        /// <param name="filePath">file path string input</param>
+        /// <param name="content">contents to be written to the file</param>
+        /// <exception cref="GPLexception"></exception>
+        public void WriteToFile(string filePath, string content)
+        {
+            try
+            {
+                // FileMode.Create will create a new file, or overwrite if the file exists
+                using (var stream = File.Open(filePath, FileMode.Create))
+                using (var writer = new StreamWriter(stream))
+                {
+                    writer.Write(content); // Changed from WriteLine to Write for more flexibility
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new GPLexception("ERROR: Cannot save the file. " + ex.Message);
+            }
+        }
     }
 }
