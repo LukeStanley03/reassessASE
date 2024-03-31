0391d4e6927e9c0635e73d8417c09da6f9ac5ca8
Luke <l.stanley9244@student.leedsbeckett.ac.uk>
2024-03-31 16:18:00 +0100

    added move to command which moves the cursor to the desired x and y position

---
+++ reassessASE/MoveToCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reassessASE
+{
+    public class MoveToCommand : ICanvasCommand
+    {
+        private int x, y;
+
+        /// <summary>
+        /// Constructor 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public MoveToCommand(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        /// <summary>
+        /// Move the cursor to position x,y
+        /// </summary>
+        public void Execute(Canvas canvas)
+        {
+            canvas.MoveTo(x, y);
+            canvas.updateCursor(); // Update cursor position after moving
+        }
+    }
+}
