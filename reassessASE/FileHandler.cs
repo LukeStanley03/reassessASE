05fc79fcb1e5e4540562c8aa9ca16bbc5897462b
Luke <l.stanley9244@student.leedsbeckett.ac.uk>
2024-03-31 13:03:10 +0100

    created a readfrom file function which reads the contents from the file

--- reassessASE/FileHandler.cs
+++ reassessASE/FileHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,32 @@ namespace reassessASE
 {
     public class FileHandler
     {
+        /// <summary>
+        /// Reading the contents from the file
+        /// </summary>
+        /// <param name="filePath">file path string input</param>
+        /// <returns></returns>
+        /// <exception cref="GPLexception"></exception>
+        public string ReadFromFile(string filePath)
+        {
+            try
+            {
+                // Ensure that the file exists before attempting to read
+                if (!File.Exists(filePath))
+                {
+                    throw new FileNotFoundException($"The file '{filePath}' was not found.");
+                }
 
+                // Use the using statement correctly for resource management
+                using (var reader = new StreamReader(filePath))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new GPLexception("ERROR: Cannot read the file. " + ex.Message);
+            }
+        }
     }
 }
