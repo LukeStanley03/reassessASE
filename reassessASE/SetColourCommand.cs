2709fff3730f3f1736272c2e993adb6c0b9bdd6f
Luke <l.stanley9244@student.leedsbeckett.ac.uk>
2024-03-31 16:09:59 +0100

    the set colour comand to either red green or blue

--- reassessASE/SetColourCommand.cs
+++ reassessASE/SetColourCommand.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace reassessASE
 {
-    internal class SetColourCommand
+    public class SetColourCommand : ICanvasCommand
     {
+        private int red;
+        private int green;
+        private int blue;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="red"></param>
+        /// <param name="green"></param>
+        /// <param name="blue"></param>
+        public SetColourCommand(int red, int green, int blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+        }
+
+        /// <summary>
+        /// Set the Red, Green and Blue colour values
+        /// </summary>
+        /// <param name="canvas"></param>
+        public void Execute(Canvas canvas)
+        {
+            canvas.SetColour(red, green, blue);
+        }
     }
+
 }
