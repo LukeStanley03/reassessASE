aedba661b565cd176fa592a9dc77173c43ba8717
Luke <l.stanley9244@student.leedsbeckett.ac.uk>
2024-03-31 16:08:07 +0100

    draws the square with the width and executes

--- reassessASE/SquareCommand.cs
+++ reassessASE/SquareCommand.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace reassessASE
 {
-    internal class SquareCommand
+    public class SquareCommand : ICanvasCommand
     {
+        private int width;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="width"></param>
+        public SquareCommand(int width)
+        {
+            this.width = width;
+        }
+
+        /// <summary>
+        /// Draw a square with width
+        /// </summary>
+        /// <param name="canvas"></param>
+        public void Execute(Canvas canvas)
+        {
+            canvas.Square(width);
+        }
     }
 }
