ff4af221d3ff563d643851e4599c4f93d64c6432
Luke <l.stanley9244@student.leedsbeckett.ac.uk>
2024-03-31 16:07:17 +0100

    draws the triangle with the width and height

--- reassessASE/TriangleCommand.cs
+++ reassessASE/TriangleCommand.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace reassessASE
 {
-    internal class TriangleCommand
+    public class TriangleCommand : ICanvasCommand
     {
+        private int width;
+        private int height;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        public TriangleCommand(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        /// <summary>
+        /// Draw a triangle with width and height
+        /// </summary>
+        /// <param name="canvas"></param>
+        public void Execute(Canvas canvas)
+        {
+            canvas.Triangle(width, height);
+        }
     }
 }
